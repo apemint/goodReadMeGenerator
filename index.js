d83ca9215e09016ba2d6fe7469cae29b5d4eacdf
apemint <escobar.andres.alfonso@gmail.com>
2020-11-07 23:31:17 -0500

    finished the js inquirer prompts and .then, seems to be working, just figuring out how to add license info through a function

--- index.js
+++ index.js
@@ -1,65 +1,110 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
 
+const inquirer = require("inquirer");
+const fs = require("fs");
 const generateReadMe = (answers) =>
-    ``
+
+    `# ${answers.title}
+
+## Description
+${answers.description}
+## Table of Contents
+[Installation](#installation)
+[Usage](#usage)
+[Contribution](#contribution)
+[License](#license)
+[Contributing](#contributing)
+[Tests](#tests)
+
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## Contribution
+${answers.contribution}
+
+## License
+This project is licensed under ${answers.license} - see the  file for details
+
+##Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+Please contact me with any questions 
+GitHub: ${answers.github}
+Email: ${answers.email}`;
+
 
 inquirer
     .prompt([
         {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of this project?'
+            type: "input",
+            name: "title",
+            message: "What is the title of this project?",
         },
         {
             type: 'input',
-            name: 'Description',
-            message: 'Give a description of the project',
+            name: 'description',
+            message: 'Give a description of the project.',
         },
         {
             type: 'input',
-            name: 'Table of Contents',
-            message: '',
+            name: 'installation',
+            message: 'How do you install?',
         },
         {
             type: 'input',
-            name: 'Installation',
-            message: 'How do you install'
+            name: 'usage',
+            message: 'How do you use this?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Any licenses used?',
+            choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
         },
         {
             type: 'input',
-            name: 'Usage',
-            message: 'How do you use?',
+            name: 'contribution',
+            message: 'Any contribution guidelines?',
         },
         {
             type: 'input',
-            name: 'License',
-            message: 'Licenses?'
+            name: 'tests',
+            message: 'Tests?',
         },
         {
             type: 'input',
-            name: 'Contributing',
-            message: '',
+            name: 'github',
+            message: 'What is your GitHub username?',
         },
         {
             type: 'input',
-            name: 'Tests',
-            message: ''
+            name: 'email',
+            message: 'What is you email?',
         },
         {
             type: 'input',
-            name: 'Questions',
-            message: '',
-        }
+            name: 'deployedProject',
+            message: " What is your deployed project URL?",
+            default: 'none',
+        },
     ])
-    .then(answers => {
-        const fileName = "";
-
-        fs.writeFile(fileName, generateReadMe(answers), (err)=> {
+    .then((answers) => {
+        const fileName = 'README.md';
+        fs.writeFile(fileName, generateReadMe(answers), (err) => {
             if (err) {
                 console.error(err);
             } else {
                 console.log("Successfully wrote to README.md");
             }
-        })
-    })
+        });
+
+    });
+
+
